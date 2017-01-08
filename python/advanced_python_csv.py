dd980b9446b8c04a7c2f9cda9c388ffc33bf02a4
nikhiliyengar <nikhil.iyengar@gmail.com>
2017-01-08 13:20:23 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+email_list = []
+fin=open('Preff.txt')
+fout = open('emails.csv','w')
+line=fin.readline() # skip the headers
+for line in fin:
+    word=line.strip() # remove EOL character
+    base_list = word.split(',') # split csv line and place in list
+    email=base_list[3]
+
+    fout.write(email)
+    fout.write("\n")
+    
+fin.close()
+fout.close()
