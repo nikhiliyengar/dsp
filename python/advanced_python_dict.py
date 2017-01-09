49dac88cc5ff723d92f501155f798c6f11ad5b09
nikhiliyengar <nikhil.iyengar@gmail.com>
2017-01-08 22:36:48 -0500

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+professor_dict = {('Susan', 'Ellenberg'): ['Ph.D.', 'Professor', 'sellenbe@upenn.edu'], ('Jonas', 'Ellenberg'): ['Ph.D.', 'Professor', 'jellenbe@mail.med.upenn.edu'], ('Yimei', 'Li'): ['Ph.D.', 'Assistant Professor', 'liy3@email.chop.edu'], ('Mingyao','Li'): ['Ph.D.', 'Associate Professor', 'mingyao@mail.med.upenn.edu'], ('Hongzhe','Li'): ['Ph.D.', 'Professor', 'hongzhe@upenn.edu'] }
+
+
+#print professor_dict
+#print "\n"
+
+prof_list = professor_dict.keys()
+prof_list.sort()
+
+#print prof_list
+
+for val in prof_list:
+    print val,professor_dict[val]
